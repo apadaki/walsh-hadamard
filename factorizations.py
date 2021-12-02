9a8e878c6cca2948e697f6db1c4dcc78aba7567c
Ashwin Padaki <apadaki22@gmail.com>
2021-12-02 13:29:14 -0500

    added bernoulli brute force method

--- factorizations.py
+++ factorizations.py
@@ -1,7 +1,8 @@
 import numpy as np
-import math
+import math, random
 import pickle
-from scipy.linalg import lu
+from numpy.random.mtrand import randint
+from scipy.linalg import lu, pinv
 
 from computations import iterative_comp
 
@@ -83,20 +84,15 @@ def evaluate_factorization(A,B,k):
     nz = np.count_nonzero(A) * np.count_nonzero(B)
     return math.log2(nz)/(2*k)
 
-if __name__ == '__main__':
-    # construct H1 through H7
-    H = [np.array([1], dtype=int)]
-    h1 = np.array([[1,1],[1,-1]])
-    for i in range(1,8):
-        H.append(np.kron(h1,H[i-1]))
-    
+
+def run_factorizations(H):
     for k in range(1,5):
         solutions_k = iterative_comp(H, k, verbose=False)
 
         sym_soln = solutions_k[0]
         for soln in solutions_k:
-            if (soln==soln.T).all():
-                print('[symmetric found]')
+            if (soln!=soln.T).all():
+                print('[non symmetric found]')
                 sym_soln = soln
                 break
 
@@ -132,3 +128,53 @@ if __name__ == '__main__':
         print('LU factorization: ', e_value)
 
         print()
+
+# generate a random sparse candidate for left factor and find e-value
+def brute_force_bernoulli(H,k):
+    A = np.matrix(np.zeros((2**k, 2**k+random.randint(1,3))))
+    num_entries_A = A.shape[0]*A.shape[1]
+    desired_nz_A = 2**k*0.9
+    # choose Bernoulli parameter based on desired sparsitys
+    p = desired_nz_A/num_entries_A
+    for i in range(A.shape[0]):
+        for j in range(A.shape[1]):
+            r = random.random()
+            if r < p:
+                A[i,j] = random.randint(0,1)*2-1
+            else:
+                A[i,j] = 0
+
+    # find Moore-Penrose pseudo-inverse of A to compute B
+    Ainv = pinv(A)
+
+    B = np.round(Ainv@H[k], decimals=4)
+
+    # verify that H[k] = AB, which is not necessarily given
+    if np.allclose(A@B, H[k]):
+        return A,B,evaluate_factorization(A,B,k)
+    return -1,-1,-1
+
+
+if __name__ == '__main__':
+    # construct H1 through H7
+    H = [np.array([1], dtype=int)]
+    h1 = np.array([[1,1],[1,-1]])
+    for i in range(1,8):
+        H.append(np.kron(h1,H[i-1]))
+
+    # run_factorizations(H)
+
+    min_A,min_B=None,None
+    min_e = 10
+    n_iter = 100000
+    for i in range(n_iter):
+        if (i != 0 and i % 10000 == 0):
+            print(i)
+        A,B,e = brute_force_bernoulli(H,3)
+        if e != -1 and e < min_e:
+            min_A = A
+            min_B = B
+            min_e = e
+    print(min_A)
+    print(min_B)
+    print(min_e)
